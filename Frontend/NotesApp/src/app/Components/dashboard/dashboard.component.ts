1448d5e6d61fdcfe03c61ca902ea50306c562de7
Malhar-Deshpande <88783651+Malhar-Deshpande@users.noreply.github.com>
2021-08-23 15:58:41 +0530

    UI for Creating note

--- Frontend/NotesApp/src/app/Components/dashboard/dashboard.component.ts
+++ Frontend/NotesApp/src/app/Components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { HostListener, Input } from '@angular/core';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,15 +9,44 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  showFiller = false;
-  @ViewChild('drawer') drawer!: ElementRef;
-
-  constructor() { }
+  isPinned: boolean = false;
+  @ViewChild('note') note!: ElementRef;
+  @ViewChild('createNoteAction') createNoteAction!: ElementRef;
+  @ViewChild('noteTitle') noteTitle!: ElementRef;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
-  hover() {
-    this.drawer.nativeElement.style
+  noteForm = this.fb.group({
+    noteTitle: [''],
+    noteContent: [''],
+  })
+
+  showTitle() {
+    this.createNoteAction.nativeElement.style.display = 'flex'
+    this.noteTitle.nativeElement.style.display = 'flex'
+  }
+
+  hideTitle() {
+    this.createNoteAction.nativeElement.style.display = 'none'
+    this.noteTitle.nativeElement.style.display = 'none'
+
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  onClick(target: any) {
+    console.log("TARGET CLASS ", target)
+    const clickedInside = this.note.nativeElement.contains(target);
+    if (!clickedInside) {
+      this.hideTitle();
+      console.log("Note Form Value ", this.noteForm.value)
+      this.noteForm.reset();
+    }
+  }
+
+  submitNote() {
+    console.log("Note Value")
+    console.log(this.noteForm.value)
   }
 }
